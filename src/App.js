993b274d49948b5738c90a7f27f6af2f068e482c
Masha Loyanich <masha@soterosoft.com>
2022-02-15 23:51:23 -0800

    Home page basic design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import logo from "./logo.png";
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        TrackMe.Ninja website
-      </h1>
+      <Navbar />
+      <div className='ninja-stub'>
+        <h1>
+          TrackMe Ninja
+        </h1>
+
+      </div>
+      <div className='app-tracker-add'>
+        <div className='add-tracker'>
+          <input type="text" className='add-tracker-input' />
+          <button className='add-tracker-button'>Track</button>
+        </div>
+        <div className='app-logo'>
+          <img src={logo} alt="logo" className='app-logo' />
+        </div>
+      </div>
     </div>
   );
 }
