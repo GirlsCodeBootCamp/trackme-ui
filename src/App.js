e7e4cadb5d0f6dde1c27201ef25c9bf5c39275fd
Masha <58474044+mloyanich@users.noreply.github.com>
2022-02-18 13:12:16 -0800

    Home page basic design (#1)
    * Home page basic design
    
    * addressing pull request comments
    Created new AddTracker component,
    switched to folder per component, as we can use separate css per component
    
    Co-authored-by: Masha Loyanich <masha@soterosoft.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
+import AddTracker from './components/AddTracker';
+import Navbar from './components/Navbar';
+import logo from "./logo.png";
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        TrackMe.Ninja website
-      </h1>
+      <header>
+        <Navbar />
+      </header>
+      <div className='main-content'>
+        <h1 className='title'>
+          TrackMe Ninja
+        </h1>
+
+        <div className='app-tracker-add'>
+          <AddTracker />
+          <div className='app-logo'>
+            <img src={logo} alt="logo" />
+          </div>
+        </div>
+      </div>
+
+      <footer>
+        &copy; Developed by Woman Career Boot Camp
+      </footer>
     </div>
   );
 }
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from "../../logo.png"
+
+export default function Navbar() {
+    return (
+        <header className="App-header">
+            <img src={logo} alt="logo" className='nav-logo' />
+            <div className='navbar-text'>Login</div>
+        </header>
+    )
+}
